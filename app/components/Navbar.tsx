ed77fef8ae5fa20e5a4eb832d57d2e003cb25585
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-12-28 20:05:49 +0700

    feat : add navbar components

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,42 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Navbar = () => {
+  const pathName = usePathname();
   return (
-    <div>
+    <div className="bg-blue-500 p-3">
       <nav>
-        <div>
-          <Link href="/">Home</Link>
-          <Link href="/about">About</Link>
-          <Link href="/product">Product</Link>
+        <div className="flex space-x-4">
+          <div
+            className={
+              pathName === "/"
+                ? "text-white bg-gray-500 px-3 py-2 rounded-md"
+                : "text-gray-300 hover:text-white px-3 py-2 rounded-md"
+            }
+          >
+            <Link href="/">Home</Link>
+          </div>
+          <div
+            className={
+              pathName === "/about"
+                ? "text-white bg-gray-500 px-3 py-2 rounded-md"
+                : "text-gray-300 hover:text-white px-3 py-2 rounded-md"
+            }
+          >
+            <Link href="/about">About</Link>
+          </div>
+          <div
+            className={
+              pathName === "/product"
+                ? "text-white bg-gray-500 px-3 py-2 rounded-md"
+                : "text-gray-300 hover:text-white px-3 py-2 rounded-md"
+            }
+          >
+            <Link href="/product">Product</Link>
+          </div>
         </div>
       </nav>
     </div>

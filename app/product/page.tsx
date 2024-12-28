2abc27f0e69e4bcfa8747d123a7e0edd72196bb5
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-12-28 20:25:57 +0700

    feat : add fetching api produck

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
-const Product = () => {
-  return <div>List Product </div>;
+interface ProductsProps {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+}
+
+const ProductPage = async () => {
+  const res = await fetch("https://fakestoreapi.com/products?limit=2");
+  const products: ProductsProps[] = await res.json();
+  console.log(products);
+  return <div>{products.map((products) => products.title)} </div>;
 };
 
-export default Product;
+export default ProductPage;

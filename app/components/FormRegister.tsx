e3507a9122a5229b08106b391d2455b6df97f1a4
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-12-30 13:54:29 +0700

    feat :: add page login and handle submit

--- app/components/FormRegister.tsx
+++ app/components/FormRegister.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 
 const FormRegister = () => {
@@ -8,14 +10,19 @@ const FormRegister = () => {
     console.log("Password", formData.get("password"));
   };
   return (
-    <div>
-      <h1></h1>
-      <form onSubmit={handleSubmit}>
+    <div className="flex flex-col items-center">
+      <h1 className="text-3xl">Register</h1>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-4 border p-2 mx-auto mt-8"
+      >
         <label htmlFor="email">Email</label>
         <input type="email" name="email" className="border" />
 
         <label htmlFor="password">Password</label>
         <input type="password" name="password" className="border" />
+
+        <button type="submit">Register</button>
       </form>
     </div>
   );
---
+++ app/login/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import FormRegister from "../components/FormRegister";
+
+const LoginPage = () => {
+  return <FormRegister />;
+};
+
+export default LoginPage;

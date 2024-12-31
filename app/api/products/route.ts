59f43e2d3486b9bb41ca2c0393be2bb2ce902970
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-12-31 10:45:35 +0700

    feat : add route.ts api json

---
+++ app/api/products/route.ts
@@ -0,0 +1,10 @@
+// GET  : to get the data
+export async function GET() {
+  return Response.json({ id: 1, name: "laptop" });
+}
+
+// POST : to create the data
+
+// PUT : to update the data
+
+// DELETE : to delete the data

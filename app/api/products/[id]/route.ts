491501a161719382a870281d4380feb3188c9b25
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2025-01-01 02:55:17 +0700

    feat : add route api id

---
+++ app/api/products/[id]/route.ts
@@ -0,0 +1,25 @@
+import { products } from "@/app/data/productData";
+import { NextRequest } from "next/server";
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await req.json();
+  const { id, title, description, price, category, image } = body;
+
+  const productIndex = products.findIndex(
+    (products) => products.id == parseInt(params.id)
+  );
+
+  products[productIndex] = {
+    id,
+    title,
+    description,
+    price,
+    category,
+    image,
+  };
+
+  return Response.json(products[productIndex], { status: 200 });
+}

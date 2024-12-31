0417045c7997c176ce52d54b676b9288450d9cb8
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-12-31 11:28:08 +0700

    feat : add post api is success

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,4 +1,5 @@
 import { products } from "@/app/data/productData";
+import { NextRequest } from "next/server";
 
 // GET  : to get the data
 export async function GET() {
@@ -6,6 +7,19 @@ export async function GET() {
 }
 
 // POST : to create the data
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const newProduct = {
+    id: products.length + 1,
+    title: body.title,
+    description: body.description,
+    price: body.price,
+    category: body.category,
+    image: body.image,
+  };
+  products.push(newProduct);
+  return Response.json(newProduct, { status: 201 });
+}
 
 // PUT : to update the data
 
